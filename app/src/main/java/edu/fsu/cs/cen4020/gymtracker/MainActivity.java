06444ea5c724e1bbb89651075278c61472d0511d
Javier Salas <JavierTSalas@users.noreply.github.com>
2019-02-18 10:29:59 -0500

    Initial commit

---
+++ app/src/main/java/edu/fsu/cs/cen4020/gymtracker/MainActivity.java
@@ -0,0 +1,48 @@
+package edu.fsu.cs.cen4020.gymtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+    private static final String TAG = MainActivity.class.getCanonicalName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // If there is no user
+        if (currentUser==null)
+        {
+            Log.d(TAG,"currentUser is null, prompting for login");
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setLogo(R.drawable.gym_tracker_logo)
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.PhoneBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }else {
+            // For the e
+            Log.d(TAG,"currentUser is "+currentUser.getUid());
+        }
+
+    }
+}
