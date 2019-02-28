7b6f0714dba2fcde51e1c8301659c787300f2309
Zachary <zlh15b@my.fsu.edu>
2019-02-28 14:20:43 -0500

    navigation drawer buttons
    All navigation drawer buttons link to their correct files.

--- app/src/main/java/edu/fsu/cs/cen4020/gymtracker/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/cen4020/gymtracker/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.fsu.cs.cen4020.gymtracker;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,7 +17,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     // Choose an arbitrary request code value
@@ -32,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open1,R.string.navigation_drawer_close);
@@ -59,6 +64,36 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG,"currentUser is "+currentUser.getUid());
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_scan:
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ScanFragment()).commit();
+                break;
+            case R.id.nav_nutrition:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NutritionFragment()).commit();
+                break;
+            case R.id.nav_calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CalendarFragment()).commit();
+                break;
+            case R.id.nav_search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SearchFragment()).commit();
+                break;
+            case R.id.nav_log:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LogFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
